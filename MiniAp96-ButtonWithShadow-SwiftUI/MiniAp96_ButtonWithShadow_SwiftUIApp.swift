9ae3244c79d99b0c0f042c9ff7f6b89081fcfbba
Kota <maedakoutaa@gmail.com>
2022-09-17 11:56:41 +0900

    Initial Commit

---
+++ MiniAp96-ButtonWithShadow-SwiftUI/MiniAp96_ButtonWithShadow_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniAp96_ButtonWithShadow_SwiftUIApp.swift
+//  MiniAp96-ButtonWithShadow-SwiftUI
+//
+//  Created by 前田航汰 on 2022/09/17.
+//
+
+import SwiftUI
+
+@main
+struct MiniAp96_ButtonWithShadow_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
